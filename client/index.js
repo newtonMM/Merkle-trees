92b2e44c43cece876392f32b14ee97de70dd724f
newtonMM <murrithinewton700@gmail.com>
2023-07-06 11:28:27 +0300

    verification using merke lists

--- client/index.js
+++ client/index.js
@@ -1,17 +1,27 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+// const MerkleTree = require('../utils/MerkleTree');
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Loretta Kozey";
 
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  console.log(root);
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    proof,
+    name,
+    root,
     // TODO: add request body parameters here!
   });
-
-  console.log({ gift });
+  console.log("name is ", gift);
+  console.log("........", proof);
 }
 
 main();
